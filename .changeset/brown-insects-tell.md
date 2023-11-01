203264375641c8dd05c07963ff71a5b21dc08411
Manrich121 <manrich121@gmail.com>
2023-11-01 11:03:44 +0200

    Fix Table.createLocalOnly and AttachmentTable constructor

---
+++ .changeset/brown-insects-tell.md
@@ -0,0 +1,5 @@
+---
+'@journeyapps/powersync-sdk-common': patch
+---
+
+Fix `Table.createLocalOnly` to not also be `insertOnly`
--- packages/powersync-attachments/src/Schema.ts
+++ packages/powersync-attachments/src/Schema.ts
@@ -1,11 +1,6 @@
-import {
-  Column,
-  ColumnType,
-  Table,
-  TableOptions,
-} from "@journeyapps/powersync-sdk-common";
+import { Column, ColumnType, Table, TableOptions } from '@journeyapps/powersync-sdk-common';
 
-export const ATTACHMENT_TABLE = "attachments";
+export const ATTACHMENT_TABLE = 'attachments';
 
 export interface AttachmentRecord {
   id: string;
@@ -22,11 +17,10 @@ export enum AttachmentState {
   QUEUED_UPLOAD = 1, // Attachment to be uploaded
   QUEUED_DOWNLOAD = 2, // Attachment to be downloaded
   SYNCED = 3, // Attachment has been synced
-  ARCHIVED = 4, // Attachment has been orphaned, i.e. the associated record has been deleted
+  ARCHIVED = 4 // Attachment has been orphaned, i.e. the associated record has been deleted
 }
 
-export interface AttachmentTableOptions
-  extends Omit<TableOptions, "name" | "columns"> {
+export interface AttachmentTableOptions extends Omit<TableOptions, 'name' | 'columns'> {
   name?: string;
   additionalColumns?: Column[];
 }
@@ -36,15 +30,17 @@ export class AttachmentTable extends Table {
     super({
       ...options,
       name: options?.name ?? ATTACHMENT_TABLE,
+      localOnly: true,
+      insertOnly: false,
       columns: [
-        new Column({ name: "filename", type: ColumnType.TEXT }),
-        new Column({ name: "local_uri", type: ColumnType.TEXT }),
-        new Column({ name: "timestamp", type: ColumnType.INTEGER }),
-        new Column({ name: "size", type: ColumnType.INTEGER }),
-        new Column({ name: "media_type", type: ColumnType.TEXT }),
-        new Column({ name: "state", type: ColumnType.INTEGER }), // Corresponds to AttachmentState
-        ...(options?.additionalColumns ?? []),
-      ],
+        new Column({ name: 'filename', type: ColumnType.TEXT }),
+        new Column({ name: 'local_uri', type: ColumnType.TEXT }),
+        new Column({ name: 'timestamp', type: ColumnType.INTEGER }),
+        new Column({ name: 'size', type: ColumnType.INTEGER }),
+        new Column({ name: 'media_type', type: ColumnType.TEXT }),
+        new Column({ name: 'state', type: ColumnType.INTEGER }), // Corresponds to AttachmentState
+        ...(options?.additionalColumns ?? [])
+      ]
     });
   }
 }
