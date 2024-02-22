6b6bd174182a8f1c1b858cc9ced4d999dbaad83c
Steven Ontong <steven@journeyapps.com>
2024-02-22 08:33:42 +0200

    added a sample test

--- packages/powersync-sdk-web/tests/main.test.ts
+++ packages/powersync-sdk-web/tests/main.test.ts
@@ -1,8 +1,12 @@
 import { afterEach, beforeEach, describe, expect, it } from 'vitest';
 import { AbstractPowerSyncDatabase, Column, ColumnType, Schema, Table } from '@journeyapps/powersync-sdk-common';
 import { WASQLitePowerSyncDatabaseOpenFactory } from '@journeyapps/powersync-sdk-web';
-
+import { v4 as uuid } from 'uuid';
 // TODO import tests from a common package
+
+type User = {
+  name: string;
+};
 
 describe('Basic', () => {
   const factory = new WASQLitePowerSyncDatabaseOpenFactory({
@@ -21,14 +25,23 @@ describe('Basic', () => {
     db = factory.getInstance();
   });
 
-  afterEach(() => {
-    db.close();
+  afterEach(async () => {
+    await db.disconnectAndClear();
+    await db.close();
   });
 
   describe('executeQuery', () => {
     it('should execute a select query using getAll', async () => {
       const result = await db.getAll('SELECT * FROM users');
       expect(result.length).toEqual(0);
+    });
+
+    it('should allow inserts', async () => {
+      const testName = 'Steven';
+      await db.execute('INSERT INTO users (id, name) VALUES(?, ?)', [uuid(), testName]);
+      const result = await db.get<User>('SELECT * FROM users');
+
+      expect(result.name).equals(testName);
     });
   });
 });
