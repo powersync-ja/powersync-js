bb603bcf002955f8b26a35de2cd2d516e38d5489
Steven Ontong <steven@journeyapps.com>
2024-02-22 16:45:48 +0200

    added crud and schema tests

--- packages/powersync-sdk-web/tests/crud.test.ts
+++ packages/powersync-sdk-web/tests/crud.test.ts
@@ -11,47 +11,27 @@ import {
   UpdateType
 } from '@journeyapps/powersync-sdk-common';
 import { WASQLitePowerSyncDatabaseOpenFactory } from '@journeyapps/powersync-sdk-web';
+import { v4 as uuid } from 'uuid';
+import { testSchema } from './test_schema';
 
 const testId = '2290de4f-0488-4e50-abed-f8e8eb1d0b42';
 
-const schema = new Schema([
-  new Table({
-    name: 'assets',
-    columns: [
-      new Column({ name: 'created_at', type: ColumnType.TEXT }),
-      new Column({ name: 'make', type: ColumnType.TEXT }),
-      new Column({ name: 'model', type: ColumnType.TEXT }),
-      new Column({ name: 'serial_number', type: ColumnType.TEXT }),
-      new Column({ name: 'quantity', type: ColumnType.INTEGER }),
-      new Column({ name: 'user_id', type: ColumnType.TEXT }),
-      new Column({ name: 'customer_id', type: ColumnType.TEXT }),
-      new Column({ name: 'description', type: ColumnType.TEXT })
-    ],
-    indexes: [
-      new Index({
-        name: 'makemodel',
-        columns: [new IndexedColumn({ name: 'make' }), new IndexedColumn({ name: 'model' })]
-      })
-    ]
-  }),
-
-  new Table({
-    name: 'customers',
-    columns: [new Column({ name: 'name', type: ColumnType.TEXT }), new Column({ name: 'email', type: ColumnType.TEXT })]
-  })
-]);
-
 describe('CRUD Tests', () => {
-  const factory = new WASQLitePowerSyncDatabaseOpenFactory({
-    dbFilename: 'test.db',
-    schema
-  });
-
   let powersync: AbstractPowerSyncDatabase;
 
   beforeEach(async () => {
-    await powersync?.disconnectAndClear();
-    powersync = factory.getInstance();
+    powersync = new WASQLitePowerSyncDatabaseOpenFactory({
+      /**
+       * Deleting the IndexDB seems to freeze the test.
+       * Use a new DB for each run to keep CRUD counters
+       * consistent
+       */
+      dbFilename: 'test.db' + uuid(),
+      schema: testSchema,
+      flags: {
+        enableMultiTabs: false
+      }
+    }).getInstance();
   });
 
   afterEach(async () => {
@@ -60,7 +40,7 @@ describe('CRUD Tests', () => {
   });
 
   it('INSERT', async () => {
-    expect(await powersync.getAll('SELECT * FROM ps_crud')).deep.equals([]);
+    expect(await powersync.getAll('SELECT * FROM ps_crud')).empty;
 
     await powersync.execute('INSERT INTO assets(id, description) VALUES(?, ?)', [testId, 'test']);
 
@@ -72,9 +52,8 @@ describe('CRUD Tests', () => {
 
     var tx = (await powersync.getNextCrudTransaction())!;
     expect(tx.transactionId).equals(1);
-    expect(tx.crud[0].equals(new CrudEntry(1, UpdateType.PUT, 'assets', testId, 1, { description: 'test' }))).equals(
-      true
-    );
+    const expectedCrudEntry = new CrudEntry(1, UpdateType.PUT, 'assets', testId, 1, { description: 'test' });
+    expect(tx.crud[0].equals(expectedCrudEntry)).true;
   });
 
   it('INSERT OR REPLACE', async () => {
@@ -112,151 +91,163 @@ describe('CRUD Tests', () => {
     ]);
 
     var tx = (await powersync.getNextCrudTransaction())!;
-    expect(tx.transactionId).equals(3);
+    expect(tx.transactionId).equals(2);
 
-    expect(tx.crud[0].equals(new CrudEntry(2, UpdateType.PATCH, 'assets', testId, 3, { description: 'test2' }))).equals(
+    const expectedCrudEntry = new CrudEntry(2, UpdateType.PATCH, 'assets', testId, 2, { description: 'test2' });
+
+    expect(tx.crud[0].equals(expectedCrudEntry)).true;
+  });
+
+  it('DELETE', async () => {
+    await powersync.execute('INSERT INTO assets(id, description, make) VALUES(?, ?, ?)', [testId, 'test', 'test']);
+    await powersync.execute('DELETE FROM ps_crud WHERE 1');
+
+    await powersync.execute('DELETE FROM assets WHERE id = ?', [testId]);
+
+    expect(await powersync.getAll('SELECT data FROM ps_crud ORDER BY id')).deep.equals([
+      { data: `{"op":"DELETE","type":"assets","id":"${testId}"}` }
+    ]);
+
+    var tx = (await powersync.getNextCrudTransaction())!;
+    expect(tx.transactionId).equals(2);
+    const expectedCrudEntry = new CrudEntry(2, UpdateType.DELETE, 'assets', testId, 2);
+    expect(tx.crud[0].equals(expectedCrudEntry)).true;
+  });
+
+  it('UPSERT not supported', async () => {
+    // Just shows that we cannot currently do this
+    expect(
+      powersync.execute('INSERT INTO assets(id, description) VALUES(?, ?) ON CONFLICT DO UPDATE SET description = ?', [
+        testId,
+        'test2',
+        'test3'
+      ])
+    ).rejects.toThrowError('cannot UPSERT a view');
+  });
+
+  it('INSERT-only tables', async () => {
+    await powersync.disconnectAndClear();
+
+    powersync = new WASQLitePowerSyncDatabaseOpenFactory({
+      /**
+       * Deleting the IndexDB seems to freeze the test.
+       * Use a new DB for each run to keep CRUD counters
+       * consistent
+       */
+      dbFilename: 'test.db' + uuid(),
+      schema: new Schema([
+        new Table({
+          name: 'logs',
+          insertOnly: true,
+          columns: [
+            new Column({ name: 'level', type: ColumnType.TEXT }),
+            new Column({ name: 'content', type: ColumnType.TEXT })
+          ]
+        })
+      ]),
+      flags: {
+        enableMultiTabs: false
+      }
+    }).getInstance();
+
+    expect(await powersync.getAll('SELECT * FROM ps_crud')).empty;
+
+    await powersync.execute('INSERT INTO logs(id, level, content) VALUES(?, ?, ?)', [testId, 'INFO', 'test log']);
+
+    expect(await powersync.getAll('SELECT data FROM ps_crud ORDER BY id')).deep.equals([
+      {
+        data: `{"op":"PUT","type":"logs","id":"${testId}","data":{"content":"test log","level":"INFO"}}`
+      }
+    ]);
+
+    expect(await powersync.getAll('SELECT * FROM logs')).empty;
+
+    var tx = (await powersync.getNextCrudTransaction())!;
+    expect(tx.transactionId).equals(1);
+    const expectedCrudEntry = new CrudEntry(1, UpdateType.PUT, 'logs', testId, 1, {
+      level: 'INFO',
+      content: 'test log'
+    });
+    expect(tx.crud[0].equals(expectedCrudEntry)).true;
+  });
+
+  it('big numbers - integer', async () => {
+    const bigNumber = 1 << 62;
+    await powersync.execute('INSERT INTO assets(id, quantity) VALUES(?, ?)', [testId, bigNumber]);
+
+    expect(await powersync.get('SELECT quantity FROM assets WHERE id = ?', [testId])).deep.equals({
+      quantity: bigNumber
+    });
+    expect(await powersync.getAll('SELECT data FROM ps_crud ORDER BY id')).deep.equals([
+      {
+        data: `{"op":"PUT","type":"assets","id":"${testId}","data":{"quantity":${bigNumber}}}`
+      }
+    ]);
+
+    var tx = (await powersync.getNextCrudTransaction())!;
+    expect(tx.transactionId).equals(1);
+
+    expect(tx.crud[0].equals(new CrudEntry(1, UpdateType.PUT, 'assets', testId, 1, { quantity: bigNumber }))).equals(
       true
     );
   });
 
-  // it('DELETE', async () => {
-  //   await powersync.execute('INSERT INTO assets(id, description, make) VALUES(?, ?, ?)', [testId, 'test', 'test']);
-  //   await powersync.execute('DELETE FROM ps_crud WHERE 1');
+  it('big numbers - text', async () => {
+    const bigNumber = 1 << 62;
+    await powersync.execute('INSERT INTO assets(id, quantity) VALUES(?, ?)', [testId, `${bigNumber}`]);
 
-  //   await powersync.execute('DELETE FROM assets WHERE id = ?', [testId]);
+    // Cast as INTEGER when querying
+    expect(await powersync.get('SELECT quantity FROM assets WHERE id = ?', [testId])).deep.equals({
+      quantity: bigNumber
+    });
 
-  //   expect(await powersync.getAll('SELECT data FROM ps_crud ORDER BY id')).equals([
-  //     { data: '{"op":"DELETE","type":"assets","id":"$testId"}' }
-  //   ]);
+    // Not cast as part of crud / persistance
+    expect(await powersync.getAll('SELECT data FROM ps_crud ORDER BY id')).deep.equals([
+      {
+        data: `{"op":"PUT","type":"assets","id":"${testId}","data":{"quantity":"${bigNumber}"}}`
+      }
+    ]);
 
-  //   var tx = (await powersync.getNextCrudTransaction())!;
-  //   expect(tx.transactionId).equals(3);
-  //   expect(tx.crud[0].equals(new CrudEntry(2, UpdateType.DELETE, 'assets', testId, 3))).equals(true);
-  // });
+    await powersync.execute('DELETE FROM ps_crud WHERE 1');
 
-  // it('UPSERT not supported', async () => {
-  //   // Just shows that we cannot currently do this
-  //   expect(
-  //     powersync.execute(
-  //       'INSERT INTO assets(id, description) VALUES(?, ?) ON CONFLICT DO UPDATE SET description = ?',
-  //       [testId, 'test2', 'test3']
-  //     )
-  //   ).throws('cannot UPSERT a view');
-  // });
+    await powersync.execute('UPDATE assets SET description = ?, quantity = quantity + 1 WHERE id = ?', [
+      'updated',
+      testId
+    ]);
 
-  // //   it('INSERT-only tables', () => {
-  // //     await powersync.disconnectAndClear();
-  // //     powersync = await setupPowerSync(
-  // //         path: path,
-  // //         schema: const Schema([
-  // //           Table.insertOnly(
-  // //               'logs', [Column.text('level'), Column.text('content')])
-  // //         ]));
-  // //     expect(await powersync.getAll('SELECT * FROM ps_crud'), equals([]));
-  // //     await powersync.execute(
-  // //         'INSERT INTO logs(id, level, content) VALUES(?, ?, ?)',
-  // //         [testId, 'INFO', 'test log']);
+    expect(await powersync.getAll('SELECT data FROM ps_crud ORDER BY id')).deep.equals([
+      {
+        data: `{"op":"PATCH","type":"assets","id":"${testId}","data":{"description":"updated","quantity":${bigNumber + 1}}}`
+      }
+    ]);
+  });
 
-  // //     expect(
-  // //         await powersync.getAll('SELECT data FROM ps_crud ORDER BY id'),
-  // //         equals([
-  // //           {
-  // //             'data':
-  // //                 '{"op":"PUT","type":"logs","id":"$testId","data":{"level":"INFO","content":"test log"}}'
-  // //           }
-  // //         ]));
+  it('Transaction grouping', async () => {
+    expect(await powersync.getAll('SELECT * FROM ps_crud')).empty;
+    await powersync.writeTransaction(async (tx) => {
+      await tx.execute('INSERT INTO assets(id, description) VALUES(?, ?)', [testId, 'test1']);
+      await tx.execute('INSERT INTO assets(id, description) VALUES(?, ?)', ['test2', 'test2']);
+    });
 
-  // //     expect(await powersync.getAll('SELECT * FROM logs'), equals([]));
+    await powersync.writeTransaction(async (tx) => {
+      await tx.execute('UPDATE assets SET description = ? WHERE id = ?', ['updated', testId]);
+    });
 
-  // //     var tx = (await powersync.getNextCrudTransaction())!;
-  // //     expect(tx.transactionId, equals(2));
-  // //     expect(
-  // //         tx.crud,
-  // //         equals([
-  // //           CrudEntry(1, UpdateType.put, 'logs', testId, 2,
-  // //               {"level": "INFO", "content": "test log"})
-  // //         ]));
-  // //   });
+    var tx1 = (await powersync.getNextCrudTransaction())!;
+    expect(tx1.transactionId).equals(1);
+    const expectedCrudEntries = [
+      new CrudEntry(1, UpdateType.PUT, 'assets', testId, 1, { description: 'test1' }),
+      new CrudEntry(2, UpdateType.PUT, 'assets', 'test2', 1, { description: 'test2' })
+    ];
 
-  // it('big numbers - integer', async () => {
-  //   const bigNumber = 1 << 62;
-  //   await powersync.execute('INSERT INTO assets(id, quantity) VALUES(?, ?)', [testId, bigNumber]);
+    expect(tx1.crud.map((entry, index) => entry.equals(expectedCrudEntries[index]))).deep.equals([true, true]);
+    await tx1.complete();
 
-  //   expect(await powersync.get('SELECT quantity FROM assets WHERE id = ?', [testId])).equals({ quantity: bigNumber });
-  //   expect(await powersync.getAll('SELECT data FROM ps_crud ORDER BY id')).equals([
-  //     {
-  //       data: `{"op":"PUT","type":"assets","id":"$testId","data":{"quantity":${bigNumber}}}`
-  //     }
-  //   ]);
-
-  //   var tx = (await powersync.getNextCrudTransaction())!;
-  //   expect(tx.transactionId).equals(1);
-
-  //   expect(tx.crud[0].equals(new CrudEntry(1, UpdateType.PUT, 'assets', testId, 1, { quantity: bigNumber }))).equals(
-  //     true
-  //   );
-  // });
-
-  // it('big numbers - text', async () => {
-  //   const bigNumber = 1 << 62;
-  //   await powersync.execute('INSERT INTO assets(id, quantity) VALUES(?, ?)', [testId, '$bigNumber']);
-
-  //   // Cast as INTEGER when querying
-  //   expect(await powersync.get('SELECT quantity FROM assets WHERE id = ?', [testId])).equals({ quantity: bigNumber });
-
-  //   // Not cast as part of crud / persistance
-  //   expect(await powersync.getAll('SELECT data FROM ps_crud ORDER BY id')).equals([
-  //     {
-  //       data: `{"op":"PUT","type":"assets","id":"$testId","data":{"quantity":"${bigNumber}"}}`
-  //     }
-  //   ]);
-
-  //   await powersync.execute('DELETE FROM ps_crud WHERE 1');
-
-  //   await powersync.execute('UPDATE assets SET description = ?, quantity = quantity + 1 WHERE id = ?', [
-  //     'updated',
-  //     testId
-  //   ]);
-
-  //   expect(await powersync.getAll('SELECT data FROM ps_crud ORDER BY id')).equals([
-  //     {
-  //       data: `{"op":"PATCH","type":"assets","id":"$testId","data":{"quantity":${bigNumber + 1},"description":"updated"}}`
-  //     }
-  //   ]);
-  // });
-
-  // it('Transaction grouping', async () => {
-  //   expect(await powersync.getAll('SELECT * FROM ps_crud')).equals([]);
-  //   await powersync.writeTransaction(async (tx) => {
-  //     await tx.execute('INSERT INTO assets(id, description) VALUES(?, ?)', [testId, 'test1']);
-  //     await tx.execute('INSERT INTO assets(id, description) VALUES(?, ?)', ['test2', 'test2']);
-  //   });
-
-  //   await powersync.writeTransaction(async (tx) => {
-  //     await tx.execute('UPDATE assets SET description = ? WHERE id = ?', ['updated', testId]);
-  //   });
-
-  //   var tx1 = (await powersync.getNextCrudTransaction())!;
-  //   expect(tx1.transactionId).equals(1);
-  //   // expect(
-  //   //     tx1.crud,
-  //   //     equals([
-  //   //       CrudEntry(1, UpdateType.put, 'assets', testId, 1,
-  //   //           {"description": "test1"}),
-  //   //       CrudEntry(2, UpdateType.put, 'assets', 'test2', 1,
-  //   //           {"description": "test2"})
-  //   //     ]));
-  //   await tx1.complete();
-
-  //   var tx2 = (await powersync.getNextCrudTransaction())!;
-  //   expect(tx2.transactionId).equals(2);
-  //   // expect(
-  //   //     tx2.crud,
-  //   //     equals([
-  //   //       CrudEntry(3, UpdateType.patch, 'assets', testId, 2,
-  //   //           {"description": "updated"}),
-  //   //     ]));
-  //   await tx2.complete();
-  //   expect(await powersync.getNextCrudTransaction()).equals(null);
-  // });
+    var tx2 = (await powersync.getNextCrudTransaction())!;
+    expect(tx2.transactionId).equals(2);
+    const expectedCrudEntry2 = new CrudEntry(3, UpdateType.PATCH, 'assets', testId, 2, { description: 'updated' });
+    expect(tx2.crud[0].equals(expectedCrudEntry2)).true;
+    await tx2.complete();
+    expect(await powersync.getNextCrudTransaction()).equals(null);
+  });
 });
