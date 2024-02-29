ceb4a1fc7c6147e6ee2a8d0987eb1809e4258e08
DominicGBauer <dominic@nomanini.com>
2024-02-29 16:41:11 +0200

    chore: update schemas

--- demos/nextjs-supabase-todolist/src/library/powersync/AppSchema.ts
+++ demos/nextjs-supabase-todolist/src/library/powersync/AppSchema.ts
@@ -3,15 +3,18 @@ import { column, Schema, TableV2 } from '@journeyapps/powersync-sdk-web';
 export const LISTS_TABLE = 'lists';
 export const TODOS_TABLE = 'todos';
 
-const todos = new TableV2({
-  list_id: column.text,
-  created_at: column.text,
-  completed_at: column.text,
-  description: column.text,
-  created_by: column.text,
-  completed_by: column.text,
-  completed: column.integer
-});
+const todos = new TableV2(
+  {
+    list_id: column.text,
+    created_at: column.text,
+    completed_at: column.text,
+    description: column.text,
+    created_by: column.text,
+    completed_by: column.text,
+    completed: column.integer
+  },
+  { indexes: { list: ['list_id'] } }
+);
 
 const lists = new TableV2({
   created_at: column.text,
