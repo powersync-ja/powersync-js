0856c02c27b63797f0d424deae37c2aa657b12b7
Loren Burton <lorendburton@gmail.com>
2024-04-15 22:17:07 -0700

    Update lodash reference in vite optimizeDeps config

--- demos/example-vite/vite.config.ts
+++ demos/example-vite/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     // Don't optimize these packages as they contain web workers and WASM files.
     // https://github.com/vitejs/vite/issues/11672#issuecomment-1415820673
     exclude: ['@journeyapps/wa-sqlite', '@journeyapps/powersync-sdk-web'],
-    include: ['object-hash', 'uuid', 'event-iterator', 'js-logger', 'lodash', 'can-ndjson-stream']
+    include: ['object-hash', 'uuid', 'event-iterator', 'js-logger', 'lodash/throttle', 'can-ndjson-stream']
   },
   plugins: [wasm(), topLevelAwait()],
   worker: {
