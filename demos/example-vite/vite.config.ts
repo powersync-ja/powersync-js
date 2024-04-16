8f358030a8bf34ee87dc633df97f58a41cdb9f2f
Christiaan Landman <christiaanlandman@Christiaans-MacBook-Pro-2.local>
2024-04-16 16:15:16 +0200

    fix: using nested include entries for demo apps

--- demos/example-vite/vite.config.ts
+++ demos/example-vite/vite.config.ts
@@ -18,7 +18,13 @@ export default defineConfig({
     // Don't optimize these packages as they contain web workers and WASM files.
     // https://github.com/vitejs/vite/issues/11672#issuecomment-1415820673
     exclude: ['@journeyapps/wa-sqlite', '@journeyapps/powersync-sdk-web'],
-    include: ['object-hash', 'uuid', 'event-iterator', 'js-logger', 'lodash/throttle', 'can-ndjson-stream']
+    include: [
+      '@journeyapps/powersync-sdk-common > uuid',
+      '@journeyapps/powersync-sdk-web > event-iterator',
+      '@journeyapps/powersync-sdk-web > js-logger',
+      '@journeyapps/powersync-sdk-web > lodash/throttle',
+      '@journeyapps/powersync-sdk-web > can-ndjson-stream'
+    ]
   },
   plugins: [wasm(), topLevelAwait()],
   worker: {
