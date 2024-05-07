128712e6c67313267759f9d51462a63a990a81e7
DominicGBauer <dominic@nomanini.com>
2024-05-07 16:02:32 +0200

    feat: add capacitor demo

---
+++ demos/example-capacitor/android/app/src/main/java/com/powersync/example/MainActivity.java
@@ -0,0 +1,5 @@
+package com.powersync.example;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ demos/example-capacitor/src/app/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import EntryPage from './page.jsx';
+import SystemProvider from '../components/providers/SystemProvider.jsx';
+
+const root = createRoot(document.getElementById('app')!);
+root.render(<App />);
+
+export function App() {
+  return (
+    <SystemProvider>
+      <EntryPage />
+    </SystemProvider>
+  );
+}
