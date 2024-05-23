37c1714bf67b5a3b0c3ef029f55ee04502a24908
Dominic Gunther Bauer <46312751+DominicGBauer@users.noreply.github.com>
2024-05-23 09:54:35 +0200

    feat: add capacitor demo (#157)
    Co-authored-by: DominicGBauer <dominic@nomanini.com>

---
+++ demos/example-capacitor/src/components/providers/SystemProvider.tsx
@@ -0,0 +1,38 @@
+import { PowerSyncContext } from '@powersync/react';
+import { WASQLitePowerSyncDatabaseOpenFactory } from '@powersync/web';
+import { CircularProgress } from '@mui/material';
+import Logger from 'js-logger';
+import React, { Suspense } from 'react';
+import { AppSchema } from '../../library/powersync/AppSchema.js';
+import { BackendConnector } from '../../library/powersync/BackendConnector.js';
+import { Capacitor } from '@capacitor/core';
+
+Logger.useDefaults();
+Logger.setLevel(Logger.DEBUG);
+
+const platform = Capacitor.getPlatform()
+const isIOs = platform === 'ios';
+// Web worker implementation does not work on iOS
+const useWebWorker = !isIOs;
+
+const powerSync = new WASQLitePowerSyncDatabaseOpenFactory({
+  dbFilename: 'powersync2.db',
+  schema: AppSchema,
+  flags: {
+    enableMultiTabs: false,
+    useWebWorker
+  }
+}).getInstance();
+const connector = new BackendConnector();
+
+powerSync.connect(connector);
+
+export const SystemProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Suspense fallback={<CircularProgress />}>
+      <PowerSyncContext.Provider value={powerSync}>{children}</PowerSyncContext.Provider>
+    </Suspense>
+  );
+};
+
+export default SystemProvider;
