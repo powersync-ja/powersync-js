7ac49111b74a98f11313041be80b555cfbafdb2b
Christiaan Landman <christiaan@journeyapps.com>
2024-06-10 10:17:17 +0200

    chore: Updated Vuetify dependency (#208)

--- demos/vue-supabase-todolist/src/plugins/vuetify.ts
+++ demos/vue-supabase-todolist/src/plugins/vuetify.ts
@@ -4,11 +4,8 @@ import 'vuetify/styles';
 
 // Composables
 import { createVuetify } from 'vuetify';
-import { VFab } from 'vuetify/labs/VFab';
-import { VEmptyState } from 'vuetify/labs/VEmptyState';
 
 export const vuetify = createVuetify({
-  components: { VFab, VEmptyState },
   theme: {
     defaultTheme: 'dark',
     themes: {
