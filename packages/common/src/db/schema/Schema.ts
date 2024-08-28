2a6a5b227c06a8c02cc93b689711ddfca4109438
DominicGBauer <dominic@nomanini.com>
2024-08-28 17:59:31 +0200

    chore: use createTable:

--- packages/common/src/db/schema/Schema.ts
+++ packages/common/src/db/schema/Schema.ts
@@ -41,14 +41,7 @@ export class Schema<S extends SchemaType = SchemaType> {
 
   private convertToClassicTables(props: S) {
     return Object.entries(props).map(([name, table]) => {
-      const convertedTable = new Table({
-        name,
-        columns: table.columns,
-        indexes: table.indexes,
-        localOnly: table.localOnly,
-        insertOnly: table.insertOnly,
-        viewName: table.viewName
-      });
+      const convertedTable = Table.createTable(name, table);
       return convertedTable;
     });
   }
