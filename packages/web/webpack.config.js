81ce61d1052cfe60f47a745ee1e626c32bf2dea3
Christiaan Landman <chriz.ek@gmail.com>
2024-09-05 15:12:34 +0200

    Fixed bson issue that was breaking single tab mode.

--- packages/web/webpack.config.js
+++ packages/web/webpack.config.js
@@ -2,6 +2,7 @@ const production = process.env.NODE_ENV === 'production';
 const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 const DeleteAssetsPlugin = require('./deletePlugin.plugin');
+const LimitChunkCountPlugin = require('webpack/lib/optimize/LimitChunkCountPlugin');
 
 module.exports = () => {
   return {
@@ -24,14 +25,24 @@ module.exports = () => {
       ]
     },
 
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        vm: require.resolve('vm-browserify')
+      },
+      alias: {
+        bson: path.resolve(__dirname, '../../node_modules/bson/lib/bson.cjs')
+      }
+    },
+
     externals: {
       '@journeyapps/wa-sqlite': '@journeyapps/wa-sqlite',
       '@journeyapps/wa-sqlite/src/examples/IDBBatchAtomicVFS.js':
         '@journeyapps/wa-sqlite/src/examples/IDBBatchAtomicVFS.js',
-
       '@powersync/common': '@powersync/common',
       'async-mutex': 'async-mutex',
-      bson: 'commonjs bson', // base 'bson' causes issues in react-native-web examples - this doesn't work when multitab is disabled.
       comlink: 'comlink',
       'js-logger': 'js-logger',
       lodash: 'lodash'
@@ -42,6 +53,9 @@ module.exports = () => {
       minimizer: [new TerserPlugin()]
     },
     plugins: [
+      new LimitChunkCountPlugin({
+        maxChunks: 1 // There are issues with loading the dynamic BSON import, it works if the bson dependency is in the index bundle file
+      }),
       new DeleteAssetsPlugin() // Add the custom plugin here
     ]
   };
