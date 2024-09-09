c04ecfc5b040ca4488d062ac28a0c818c3018a44
stevensJourney <51082125+stevensJourney@users.noreply.github.com>
2024-09-09 10:23:34 +0200

    [Fix] Custom Query Executor Hooks (#295)

---
+++ .changeset/orange-points-speak.md
@@ -0,0 +1,6 @@
+---
+'@powersync/react': patch
+'@powersync/vue': patch
+---
+
+React and Vue helpers should execute queries from compatible query executor methods. This should allow Kysely queries with plugins to function correctly.
--- packages/kysely-driver/src/sqlite/db.ts
+++ packages/kysely-driver/src/sqlite/db.ts
@@ -1,8 +1,15 @@
-import { PowerSyncDialect } from './sqlite-dialect';
-import { Kysely, type KyselyConfig } from 'kysely';
 import { type AbstractPowerSyncDatabase } from '@powersync/common';
+import { Dialect, Kysely, type KyselyConfig } from 'kysely';
+import { PowerSyncDialect } from './sqlite-dialect';
 
-export const wrapPowerSyncWithKysely = <T>(db: AbstractPowerSyncDatabase, options?: KyselyConfig) => {
+/**
+ * An extension of {@link KyselyConfig} which uses the {@link PowerSyncDialect} by default.
+ */
+export type PowerSyncKyselyOptions = Omit<KyselyConfig, 'dialect'> & {
+  dialect?: Dialect;
+};
+
+export const wrapPowerSyncWithKysely = <T>(db: AbstractPowerSyncDatabase, options?: PowerSyncKyselyOptions) => {
   return new Kysely<T>({
     dialect: new PowerSyncDialect({
       db
