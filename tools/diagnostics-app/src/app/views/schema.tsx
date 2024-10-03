4056a5e00d7a5742181b634b02c922b9da8b9cb4
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2024-10-03 19:21:24 +0200

    feat(diagnostics): update autogenerated schema

--- tools/diagnostics-app/src/app/views/schema.tsx
+++ tools/diagnostics-app/src/app/views/schema.tsx
@@ -4,10 +4,13 @@ import { Box, Grid, styled } from '@mui/material';
 
 export default function SchemaPage() {
   const schema = schemaManager.schemaToString();
-  const docs = `// This displays the inferred schema currently used by the diagnostics app.
-// This is based on downloaded data, rather than the source database.
-// If a table is empty, it will not display here.
-// Tables and columns are only added here. Nothing is removed until the database is cleared.`;
+  const docs = `/**
+ * This is the inferred schema of the data received by the diagnostics app.
+ * Because this schema is generated on-the-fly based on the data received by the app, it can
+ * be incomplete and should NOT be relied upon as a source of truth for your app schema.
+ * If a table is empty, it will not be shown here.
+ * Tables and columns are only added here. Nothing is removed until the database is cleared.
+ */`;
   return (
     <NavigationPage title="Dynamic Schema">
       <S.MainContainer>
