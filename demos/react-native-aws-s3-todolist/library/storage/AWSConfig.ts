58e5c992304340108e23d319e69287dac1b5a5fd
Hein <hein.vonstein@journeyapps.com>
2024-10-31 09:38:32 +0200

    Remove s3 demo dir

--- demos/react-native-aws-s3-todolist/library/storage/AWSConfig.ts
+++
@@ -1,6 +0,0 @@
-export const AWSConfig = {
-  region: process.env.EXPO_PUBLIC_AWS_S3_REGION,
-  accessKeyId: process.env.EXPO_PUBLIC_AWS_S3_ACCESS_KEY_ID || '',
-  secretAccessKey: process.env.EXPO_PUBLIC_AWS_S3_ACCESS_SECRET_ACCESS_KEY || '',
-  bucketName: process.env.EXPO_PUBLIC_AWS_S3_BUCKET_NAME || ''
-};
--- demos/react-native-aws-s3-todolist/library/supabase/AppConfig.ts
+++
@@ -1,5 +0,0 @@
-export const AppConfig = {
-  supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
-  supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-  powersyncUrl: process.env.EXPO_PUBLIC_POWERSYNC_URL
-};
