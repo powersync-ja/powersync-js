97aa39b3ab8d6f056eaad2c4edf07e8a2e8a937e
Christiaan Landman <chriz.ek@gmail.com>
2024-11-11 10:12:39 +0200

    Basic tests for Drizzle ORM package.

---
+++ packages/drizzle-driver/tests/setup/db.ts
@@ -0,0 +1,32 @@
+import { Schema, PowerSyncDatabase, column, Table, AbstractPowerSyncDatabase } from '@powersync/web';
+import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { wrapPowerSyncWithDrizzle, PowerSyncSQLiteDatabase } from '../../src/sqlite/db';
+
+const users = new Table({
+  name: column.text
+});
+
+export const drizzleUsers = sqliteTable('users', {
+  id: text('id').primaryKey().notNull(),
+  name: text('name').notNull()
+});
+
+export const TestSchema = new Schema({ users });
+export const DrizzleSchema = { users: drizzleUsers };
+
+export const getPowerSyncDb = () => {
+  const database = new PowerSyncDatabase({
+    database: {
+      dbFilename: 'test.db'
+    },
+    schema: TestSchema
+  });
+
+  return database;
+};
+
+export const getDrizzleDb = (db: AbstractPowerSyncDatabase) => {
+  const database = wrapPowerSyncWithDrizzle(db, { schema: DrizzleSchema });
+
+  return database;
+};
