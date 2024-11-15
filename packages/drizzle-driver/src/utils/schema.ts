34e8becc7e7d682ab4f016eda0ce035a3aafa481
Christiaan Landman <chriz.ek@gmail.com>
2024-11-15 16:53:25 +0200

    Transforming index options from Drizzle tables. Using getTableConfig instead of iterating keys.

--- packages/drizzle-driver/src/utils/schema.ts
+++ packages/drizzle-driver/src/utils/schema.ts
@@ -1,5 +1,6 @@
-import { column, Table, type BaseColumnType, type TableV2Options } from '@powersync/common';
+import { column, IndexShorthand, Table, type BaseColumnType, type TableV2Options } from '@powersync/common';
 import {
+  getTableConfig,
   SQLiteInteger,
   SQLiteReal,
   SQLiteText,
@@ -7,16 +8,21 @@ import {
   type TableConfig
 } from 'drizzle-orm/sqlite-core';
 
-export function toPowerSyncTable<T extends TableConfig>(table: SQLiteTableWithColumns<T>, options?: TableV2Options) {
+export function toPowerSyncTable<T extends TableConfig>(
+  table: SQLiteTableWithColumns<T>,
+  options?: Omit<TableV2Options, 'indexes'>
+) {
+  const { columns: drizzleColumns, indexes: drizzleIndexes } = getTableConfig(table);
+
   const columns: { [key: string]: BaseColumnType<number | string | null> } = {};
-  for (const [columnName, columnValue] of Object.entries(table)) {
+  for (const drizzleColumn of drizzleColumns) {
     // Skip the id column
-    if (columnName === 'id') {
+    if (drizzleColumn.name === 'id') {
       continue;
     }
 
     let mappedType: BaseColumnType<number | string | null>;
-    switch (columnValue.columnType) {
+    switch (drizzleColumn.columnType) {
       case SQLiteText.name:
         mappedType = column.text;
         break;
@@ -27,9 +33,23 @@ export function toPowerSyncTable<T extends TableConfig>(table: SQLiteTableWithCo
         mappedType = column.real;
         break;
       default:
-        throw new Error(`Unsupported column type: ${columnValue.columnType}`);
+        throw new Error(`Unsupported column type: ${drizzleColumn.columnType}`);
     }
-    columns[columnName] = mappedType;
+    columns[drizzleColumn.name] = mappedType;
   }
-  return new Table(columns, options);
+  const indexes: IndexShorthand = {};
+
+  for (const index of drizzleIndexes) {
+    index.config;
+    if (!index.config.columns.length) {
+      continue;
+    }
+    const columns: string[] = [];
+    for (const indexColumn of index.config.columns) {
+      columns.push((indexColumn as { name: string }).name);
+    }
+
+    indexes[index.config.name] = columns;
+  }
+  return new Table(columns, { ...options, indexes });
 }
