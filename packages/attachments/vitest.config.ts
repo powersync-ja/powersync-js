91a57d774d7da4bf8662e9336902fcc4076e5d74
Hein <hein.vonstein@journeyapps.com>
2024-12-03 14:10:10 +0200

    Remove unnecessary deps

--- packages/attachments/vitest.config.ts
+++ packages/attachments/vitest.config.ts
@@ -1,18 +1,8 @@
-import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import { defineConfig, UserConfigExport } from 'vitest/config';
 
 const config: UserConfigExport = {
-  worker: {
-    format: 'es',
-    plugins: () => [wasm(), topLevelAwait()]
-  },
-  optimizeDeps: {
-    // Don't optimise these packages as they contain web workers and WASM files.
-    // https://github.com/vitejs/vite/issues/11672#issuecomment-1415820673
-    exclude: ['@journeyapps/wa-sqlite', '@powersync/web']
-  },
-  plugins: [wasm(), topLevelAwait()],
+  plugins: [topLevelAwait()],
   test: {
     isolate: false,
     globals: true,
