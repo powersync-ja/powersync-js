787369455b1447c09d9821a11a54af8cec6848de
Simon Binder <simon@journeyapps.com>
2025-03-05 09:39:52 +0100

    Reduce API surface

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,8 +1,4 @@
 // Re export to only require one import in client side code
 export * from '@powersync/common';
 
-export * from './db/BetterSQLite3DBAdapter.js';
 export * from './db/PowerSyncDatabase.js';
-
-export * from './sync/stream/NodeRemote.js';
-export * from './sync/stream/NodeStreamingSyncImplementation.js';
