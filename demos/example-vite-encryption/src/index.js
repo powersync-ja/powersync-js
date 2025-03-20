098934affd11615f623a3fa3070df55375bdd166
Christiaan Landman <christiaan@journeyapps.com>
2025-03-20 18:00:44 +0200

    demo: Vite Web Encryption Example (#538)
    Co-authored-by: stevensJourney <51082125+stevensJourney@users.noreply.github.com>

---
+++ demos/example-vite-encryption/src/index.js
@@ -0,0 +1,37 @@
+import { column, Schema, Table, PowerSyncDatabase } from '@powersync/web';
+import Logger from 'js-logger';
+
+Logger.useDefaults();
+
+const customers = new Table({ name: column.text });
+
+export const AppSchema = new Schema({ customers });
+
+let PowerSync;
+
+const openDatabase = async (encryptionKey) => {
+  PowerSync = new PowerSyncDatabase({
+    schema: AppSchema,
+    database: { dbFilename: 'example-encryption.db' },
+    encryptionKey: encryptionKey
+  });
+
+  await PowerSync.init();
+
+  // Run local statements.
+  await PowerSync.execute('INSERT INTO customers(id, name) VALUES(uuid(), ?)', ['Fred']);
+
+  const result = await PowerSync.getAll('SELECT * FROM customers');
+  console.log('contents of customers: ', result);
+  console.table(result);
+};
+
+document.addEventListener('DOMContentLoaded', async (event) => {
+  let encryptionKey = '';
+
+  while (!encryptionKey) {
+    encryptionKey = prompt('Enter encryption key (non-empty string):');
+  }
+
+  openDatabase(encryptionKey);
+});
