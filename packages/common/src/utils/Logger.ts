7554de42bcd56bcf167a18ef0720c1e35bb35ef7
Christiaan Landman <chriz.ek@gmail.com>
2025-04-08 15:22:37 +0200

    Exporting logger helper to avoid having to install js-logger in projects/demos.

---
+++ packages/common/src/utils/Logger.ts
@@ -0,0 +1,32 @@
+import Logger, { ILogger, ILogLevel } from 'js-logger';
+
+export { GlobalLogger, ILogger, ILoggerOpts, ILogHandler, ILogLevel } from 'js-logger';
+
+const TypedLogger: ILogger = Logger as any;
+
+export const LogLevels = {
+  TRACE: TypedLogger.TRACE,
+  DEBUG: TypedLogger.DEBUG,
+  INFO: TypedLogger.INFO,
+  TIME: TypedLogger.TIME,
+  WARN: TypedLogger.WARN,
+  ERROR: TypedLogger.ERROR,
+  OFF: TypedLogger.OFF
+};
+
+export interface CreateLoggerOptions {
+  logLevel?: ILogLevel;
+}
+
+export function createBaseLogger() {
+  return Logger;
+}
+
+export function createLogger(name: string, options: CreateLoggerOptions = {}): ILogger {
+  const logger = Logger.get(name);
+  if (options.logLevel) {
+    logger.setLevel(options.logLevel);
+  }
+
+  return logger;
+}
