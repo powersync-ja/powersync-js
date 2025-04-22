4a19aa67749f3d045d3c793d2b939b608001febc
Christiaan Landman <chriz.ek@gmail.com>
2025-04-22 09:08:20 +0200

    Added minor documentation.

--- packages/common/src/utils/Logger.ts
+++ packages/common/src/utils/Logger.ts
@@ -18,10 +18,25 @@ export interface CreateLoggerOptions {
   logLevel?: ILogLevel;
 }
 
+/**
+ * Retrieves the base (default) logger instance.
+ *
+ * This base logger controls the default logging configuration and is shared
+ * across all loggers created with `createLogger`. Adjusting settings on this
+ * base logger affects all loggers derived from it unless explicitly overridden.
+ *
+ */
 export function createBaseLogger() {
   return Logger;
 }
 
+/**
+ * Creates and configures a new named logger based on the base logger.
+ *
+ * Named loggers allow specific modules or areas of your application to have
+ * their own logging levels and behaviors. These loggers inherit configuration
+ * from the base logger by default but can override settings independently.
+ */
 export function createLogger(name: string, options: CreateLoggerOptions = {}): ILogger {
   const logger = Logger.get(name);
   if (options.logLevel) {
