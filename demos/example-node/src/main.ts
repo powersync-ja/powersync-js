6cadbc23487f9a88e645c6e070edf07fec179c37
Christiaan Landman <chriz.ek@gmail.com>
2025-05-29 13:27:51 +0200

    WIP support

--- demos/example-node/src/main.ts
+++ demos/example-node/src/main.ts
@@ -3,19 +3,20 @@ import repl_factory from 'node:repl';
 
 import { createBaseLogger, createLogger, PowerSyncDatabase, SyncStreamConnectionMethod } from '@powersync/node';
 import { exit } from 'node:process';
-import { AppSchema, DemoConnector } from './powersync.js';
+import { AppSchema } from './powersync.js';
+import { SupabaseConnector } from './SupabaseConnector.js';
 
 const main = async () => {
   const baseLogger = createBaseLogger();
   const logger = createLogger('PowerSyncDemo');
   baseLogger.useDefaults({ defaultLevel: logger.WARN });
 
-  if (!('BACKEND' in process.env) || !('SYNC_SERVICE' in process.env)) {
-    console.warn(
-      'Set the BACKEND and SYNC_SERVICE environment variables to point to a sync service and a running demo backend.'
-    );
-    return;
-  }
+  // if (!('BACKEND' in process.env) || !('SYNC_SERVICE' in process.env)) {
+  //   console.warn(
+  //     'Set the BACKEND and SYNC_SERVICE environment variables to point to a sync service and a running demo backend.'
+  //   );
+  //   return;
+  // }
 
   const db = new PowerSyncDatabase({
     schema: AppSchema,
@@ -26,7 +27,15 @@ const main = async () => {
   });
   console.log(await db.get('SELECT powersync_rs_version();'));
 
-  await db.connect(new DemoConnector(), { connectionMethod: SyncStreamConnectionMethod.HTTP });
+  db.registerListener({
+    statusChanged(status) {
+      console.log('status changed', status);
+    }
+  });
+
+  const connector = new SupabaseConnector();
+  await connector.login();
+  await db.connect(connector, { connectionMethod: SyncStreamConnectionMethod.HTTP });
   await db.waitForFirstSync();
   console.log('First sync complete!');
 
--- demos/example-node/src/powersync.ts
+++ demos/example-node/src/powersync.ts
@@ -1,48 +1,48 @@
 import { AbstractPowerSyncDatabase, column, PowerSyncBackendConnector, Schema, Table } from '@powersync/node';
 
-export class DemoConnector implements PowerSyncBackendConnector {
-  async fetchCredentials() {
-    const response = await fetch(`${process.env.BACKEND}/api/auth/token`);
-    if (response.status != 200) {
-      throw 'Could not fetch token';
-    }
+// export class DemoConnector implements PowerSyncBackendConnector {
+//   async fetchCredentials() {
+//     const response = await fetch(`${process.env.BACKEND}/api/auth/token`);
+//     if (response.status != 200) {
+//       throw 'Could not fetch token';
+//     }
 
-    const { token } = await response.json();
+//     const { token } = await response.json();
 
-    return {
-      endpoint: process.env.SYNC_SERVICE!,
-      token: token
-    };
-  }
+//     return {
+//       endpoint: process.env.SYNC_SERVICE!,
+//       token: token
+//     };
+//   }
 
-  async uploadData(database: AbstractPowerSyncDatabase) {
-    const batch = await database.getCrudBatch();
-    if (batch == null) {
-      return;
-    }
+//   async uploadData(database: AbstractPowerSyncDatabase) {
+//     const batch = await database.getCrudBatch();
+//     if (batch == null) {
+//       return;
+//     }
 
-    const entries: any[] = [];
-    for (const op of batch.crud) {
-      entries.push({
-        table: op.table,
-        op: op.op,
-        id: op.id,
-        data: op.opData
-      });
-    }
+//     const entries: any[] = [];
+//     for (const op of batch.crud) {
+//       entries.push({
+//         table: op.table,
+//         op: op.op,
+//         id: op.id,
+//         data: op.opData
+//       });
+//     }
 
-    const response = await fetch(`${process.env.BACKEND}/api/data/`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ batch: entries })
-    });
-    if (response.status !== 200) {
-      throw new Error(`Server returned HTTP ${response.status}: ${await response.text()}`);
-    }
+//     const response = await fetch(`${process.env.BACKEND}/api/data/`, {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify({ batch: entries })
+//     });
+//     if (response.status !== 200) {
+//       throw new Error(`Server returned HTTP ${response.status}: ${await response.text()}`);
+//     }
 
-    await batch?.complete();
-  }
-}
+//     await batch?.complete();
+//   }
+// }
 
 export const LIST_TABLE = 'lists';
 export const TODO_TABLE = 'todos';
