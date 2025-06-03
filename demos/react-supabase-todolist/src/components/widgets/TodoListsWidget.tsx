05f50f1f35fcea7591b71ffc6dc8f6f9bec3fc87
stevensJourney <steven@journeyapps.com>
2025-06-03 17:38:32 +0200

    Add demo for instant query caching with localStorage

--- demos/react-supabase-todolist/src/components/widgets/TodoListsWidget.tsx
+++ demos/react-supabase-todolist/src/components/widgets/TodoListsWidget.tsx
@@ -1,7 +1,6 @@
-import { LISTS_TABLE, ListRecord, TODOS_TABLE } from '@/library/powersync/AppSchema';
 import { List } from '@mui/material';
-import { useQuery } from '@powersync/react';
-import { ArrayComparator } from '@powersync/web';
+import { useWatchedQuerySubscription } from '@powersync/react';
+import { useQueryStore } from '../providers/SystemProvider';
 import { ListItemWidget } from './ListItemWidget';
 
 export type TodoListsWidgetProps = {
@@ -13,35 +12,10 @@ const description = (total: number, completed: number = 0) => {
 };
 
 export function TodoListsWidget(props: TodoListsWidgetProps) {
-  const { data: listRecords, isLoading } = useQuery<ListRecord & { total_tasks: number; completed_tasks: number }>(
-    /* sql */ `
-      SELECT
-        ${LISTS_TABLE}.*,
-        COUNT(${TODOS_TABLE}.id) AS total_tasks,
-        SUM(
-          CASE
-            WHEN ${TODOS_TABLE}.completed = true THEN 1
-            ELSE 0
-          END
-        ) as completed_tasks
-      FROM
-        ${LISTS_TABLE}
-        LEFT JOIN ${TODOS_TABLE} ON ${LISTS_TABLE}.id = ${TODOS_TABLE}.list_id
-      GROUP BY
-        ${LISTS_TABLE}.id;
-    `,
-    [],
-    {
-      processor: {
-        mode: 'comparison',
-        comparator: new ArrayComparator({
-          compareBy: (item) => JSON.stringify(item)
-        })
-      }
-    }
-  );
+  const queries = useQueryStore();
+  const { data: listRecords, isLoading } = useWatchedQuerySubscription(queries!.lists);
 
-  if (isLoading) {
+  if (isLoading && listRecords.length == 0) {
     return <div>Loading...</div>;
   }
 
