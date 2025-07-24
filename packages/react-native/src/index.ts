c8a9af065f68a14552181d59e9e57d75dce0b053
Christiaan Landman <chriz.ek@gmail.com>
2025-07-24 14:55:47 +0200

    Removed dev package re-export from React-Native.

--- packages/react-native/src/index.ts
+++ packages/react-native/src/index.ts
@@ -1,6 +1,5 @@
 // Re export to only require one import in client side code
 export * from '@powersync/common';
-export * from '@powersync/dev';
 export * from '@powersync/react';
 
 export * from './db/PowerSyncDatabase';
