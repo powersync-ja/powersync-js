a600aa65a3f337b5b9499ca1ae71225eacb76b49
Simon Binder <simon@journeyapps.com>
2025-09-17 16:55:35 +0200

    Disable parallelism for node tests

--- packages/node/vitest.config.ts
+++ packages/node/vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vitest/config';
 // We need to define an empty config to be part of the vitest works
 export default defineConfig({
   test: {
-    silent: false
+    silent: false,
+    // This doesn't make the tests considerably slower. It may improve reliability for GH actions.
+    fileParallelism: false
   }
 });
