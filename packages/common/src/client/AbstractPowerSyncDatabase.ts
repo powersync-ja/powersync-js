d193c18da6b3767d9c4a9223a071729a78691862
Amine <amine@powersync.com>
2025-10-02 17:37:37 +0800

    docs: document the connector getter

--- packages/common/src/client/AbstractPowerSyncDatabase.ts
+++ packages/common/src/client/AbstractPowerSyncDatabase.ts
@@ -193,6 +193,11 @@ export abstract class AbstractPowerSyncDatabase extends BaseObserver<PowerSyncDB
     return this.connectionManager.syncStreamImplementation;
   }
 
+  /**
+   * The connector and connection options used to connect to the PowerSync service.
+   * 
+   * @returns The connector and connection options used to connect to the PowerSync service or null if `connect()` has not been called.
+   */
   get connector() {
     return this.connectionManager.connector;
   }
