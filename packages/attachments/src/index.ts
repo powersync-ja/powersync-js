ecb71cba0e0ec162e6826e718bf5205ee7bab3b7
joshua-journey-apps <joshua@journeyapps.com>
2025-10-07 12:23:31 +0200

    Fix exporting node and index db storage adapters

--- packages/attachments/src/index.ts
+++ packages/attachments/src/index.ts
@@ -1,5 +1,7 @@
 export * from './Schema.js';
 export * from './LocalStorageAdapter.js';
+export * from './storageAdapters/NodeFileSystemAdapter.js';
+export * from './storageAdapters/IndexDBFileSystemAdapter.js';
 export * from './RemoteStorageAdapter.js';
 export * from './AttachmentContext.js';
 export * from './StorageService.js';
