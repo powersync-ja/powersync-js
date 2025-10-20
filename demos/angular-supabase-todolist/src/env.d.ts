e549509953185359521c59a54e196b28fb694e1c
stevensJourney <51082125+stevensJourney@users.noreply.github.com>
2025-10-20 06:28:54 -0600

    chore: update Angular demo build (#738)

--- demos/angular-supabase-todolist/src/env.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  // These are injected in the Webpack config
-  interface ProcessEnv {
-    WEBPACK_PUBLIC_SUPABASE_URL: string;
-    WEBPACK_PUBLIC_SUPABASE_ANON_KEY: string;
-    WEBPACK_PUBLIC_POWERSYNC_URL: string;
-  }
-}
