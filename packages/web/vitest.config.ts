b8a91b1456055d13029677b2c5546fe27790a41c
stevensJourney <steven@journeyapps.com>
2025-10-30 10:39:57 +0200

    Vitest 4 upgrade

--- packages/web/vitest.config.ts
+++ packages/web/vitest.config.ts
@@ -1,70 +1,73 @@
 import path from 'path';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
-import { defineConfig, UserConfigExport } from 'vitest/config';
+import { defineConfig, ViteUserConfigExport } from 'vitest/config';
 
-const config: UserConfigExport = {
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp'
-    }
-  },
-  // This is only needed for local tests to resolve the package name correctly
-  resolve: {
-    alias: {
-      /**
-       * Note that this requires the Typescript to be compiled with `tsc`
-       * first. This is required due to the format of Webworker URIs
-       * they link to `.js` files.
-       */
-      '@powersync/web': path.resolve(__dirname, './lib/src'),
-      // https://jira.mongodb.org/browse/NODE-5773
-      bson: require.resolve('bson')
-    }
-  },
-  worker: {
-    format: 'es',
-    plugins: () => [wasm(), topLevelAwait()]
-  },
-  optimizeDeps: {
-    // Don't optimise these packages as they contain web workers and WASM files.
-    // https://github.com/vitejs/vite/issues/11672#issuecomment-1415820673
-    exclude: ['@journeyapps/wa-sqlite', '@powersync/web'],
-    include: ['bson', 'comlink', 'async-mutex']
-  },
-  plugins: [wasm(), topLevelAwait()],
-  test: {
-    globals: true,
-    include: ['tests/**/*.test.ts'],
-    maxConcurrency: 1,
-    // This doesn't currently seem to work in browser mode, but setting this for one day when it does
-    sequence: {
-      shuffle: false, // Disable shuffling of test files
-      concurrent: false // Run test files sequentially
+export default defineConfig(async () => {
+  // Import of an ES module is not directly supported since we don't yet define this package directly as an ES module
+  const { playwright } = await import('@vitest/browser-playwright');
+
+  return {
+    server: {
+      headers: {
+        'Cross-Origin-Opener-Policy': 'same-origin',
+        'Cross-Origin-Embedder-Policy': 'require-corp'
+      }
     },
-    browser: {
-      enabled: true,
-      /**
-       * Starts each test in a new iFrame
-       */
-      isolate: true,
-      provider: 'playwright',
-      headless: true,
-      instances: [
-        {
-          browser: 'chromium'
-        }
-        // {
-        //   browser: 'firefox'
-        // }
-        // This requires some additional work to get all tests passing
-        // {
-        //   browser: 'webkit'
-        // }
-      ]
+    // This is only needed for local tests to resolve the package name correctly
+    resolve: {
+      alias: {
+        /**
+         * Note that this requires the Typescript to be compiled with `tsc`
+         * first. This is required due to the format of Webworker URIs
+         * they link to `.js` files.
+         */
+        '@powersync/web': path.resolve(__dirname, './lib/src'),
+        // https://jira.mongodb.org/browse/NODE-5773
+        bson: require.resolve('bson')
+      }
+    },
+    worker: {
+      format: 'es',
+      plugins: () => [wasm(), topLevelAwait()]
+    },
+    optimizeDeps: {
+      // Don't optimise these packages as they contain web workers and WASM files.
+      // https://github.com/vitejs/vite/issues/11672#issuecomment-1415820673
+      exclude: ['@journeyapps/wa-sqlite', '@powersync/web'],
+      include: ['bson', 'comlink', 'async-mutex']
+    },
+    plugins: [wasm(), topLevelAwait()],
+    test: {
+      globals: true,
+      include: ['tests/**/*.test.ts'],
+      maxConcurrency: 1,
+      // This doesn't currently seem to work in browser mode, but setting this for one day when it does
+      sequence: {
+        shuffle: false, // Disable shuffling of test files
+        concurrent: false // Run test files sequentially
+      },
+      browser: {
+        enabled: true,
+        /**
+         * Starts each test in a new iFrame
+         */
+        isolate: true,
+        provider: playwright(),
+        headless: true,
+        instances: [
+          {
+            browser: 'chromium'
+          }
+          // {
+          //   browser: 'firefox'
+          // }
+          // This requires some additional work to get all tests passing
+          // {
+          //   browser: 'webkit'
+          // }
+        ]
+      }
     }
-  }
-};
-
-export default defineConfig(config);
+  } satisfies ViteUserConfigExport;
+});
--- tools/import-tests/vitest.config.ts
+++ tools/import-tests/vitest.config.ts
@@ -1,34 +1,35 @@
 import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
-import { defineConfig, UserConfigExport } from 'vitest/config';
+import { defineConfig, ViteUserConfigExport } from 'vitest/config';
 
-const config: UserConfigExport = {
-  worker: {
-    format: 'es',
-    plugins: () => [wasm(), topLevelAwait()]
-  },
-  optimizeDeps: {
-    // Don't optimise these packages as they contain web workers and WASM files.
-    // https://github.com/vitejs/vite/issues/11672#issuecomment-1415820673
-    exclude: ['@journeyapps/wa-sqlite', '@powersync/web'],
-    include: ['async-mutex', 'comlink', 'bson']
-  },
-  plugins: [wasm(), topLevelAwait()],
-  test: {
-    isolate: false,
-    globals: true,
-    include: ['src/**/*.test.ts'],
-    browser: {
-      enabled: true,
-      headless: true,
-      provider: 'playwright',
-      instances: [
-        {
-          browser: 'chromium'
-        }
-      ]
+export default defineConfig(async () => {
+  const { playwright } = await import('@vitest/browser-playwright');
+  return {
+    worker: {
+      format: 'es',
+      plugins: () => [wasm(), topLevelAwait()]
+    },
+    optimizeDeps: {
+      // Don't optimise these packages as they contain web workers and WASM files.
+      // https://github.com/vitejs/vite/issues/11672#issuecomment-1415820673
+      exclude: ['@journeyapps/wa-sqlite', '@powersync/web'],
+      include: ['async-mutex', 'comlink', 'bson']
+    },
+    plugins: [wasm(), topLevelAwait()],
+    test: {
+      isolate: false,
+      globals: true,
+      include: ['src/**/*.test.ts'],
+      browser: {
+        enabled: true,
+        headless: true,
+        provider: playwright(),
+        instances: [
+          {
+            browser: 'chromium'
+          }
+        ]
+      }
     }
-  }
-};
-
-export default defineConfig(config);
+  } satisfies ViteUserConfigExport;
+});
