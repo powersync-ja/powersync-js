533151aca5d70c8f7555111b67ca61924d47f387
Simon Binder <simon@journeyapps.com>
2025-11-03 11:04:53 +0100

    Reduce bundling code

--- packages/common/rollup.config.mjs
+++ packages/common/rollup.config.mjs
@@ -1,30 +1,49 @@
+import * as path from 'node:path';
+
 import commonjs from '@rollup/plugin-commonjs';
+import inject from '@rollup/plugin-inject';
 import json from '@rollup/plugin-json';
 import nodeResolve from '@rollup/plugin-node-resolve';
-import { nodeExternals } from 'rollup-plugin-node-externals';
 import { dts } from 'rollup-plugin-dts';
+
+function defineBuild(isNode) {
+  const suffix = isNode ? '.node' : '';
+
+  return {
+    input: 'lib/index.js',
+    output: [
+      {
+        file: `dist/bundle${suffix}.mjs`,
+        format: 'esm',
+        sourcemap: true
+      },
+      {
+        file: `dist/bundle${suffix}.cjs`,
+        format: 'cjs',
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      [
+        json(),
+        nodeResolve({ preferBuiltins: false, browser: true }),
+        commonjs({}),
+        inject({
+          Buffer: isNode ? 'node:buffer' : path.resolve('lib/buffer.js')
+        })
+      ]
+    ],
+    external: ['async-mutex', 'bson', 'buffer/', 'event-iterator']
+  };
+}
 
 /**
  * @returns {import('rollup').RollupOptions}
  */
-export default (commandLineArgs) => {
+export default () => {
   return [
-    {
-      input: 'lib/index.js',
-      output: [
-        {
-          file: 'dist/bundle.mjs',
-          format: 'esm',
-          sourcemap: true
-        },
-        {
-          file: 'dist/bundle.cjs',
-          format: 'cjs',
-          sourcemap: true
-        }
-      ],
-      plugins: [json(), nodeResolve({ preferBuiltins: false, browser: true }), commonjs({}), nodeExternals()]
-    },
+    defineBuild(false),
+    defineBuild(true),
     {
       input: './lib/index.d.ts',
       output: [{ file: 'dist/index.d.cts', format: 'cjs' }],
---
+++ packages/common/src/buffer.ts
@@ -0,0 +1,3 @@
+import { Buffer } from 'buffer/';
+
+export default Buffer;
