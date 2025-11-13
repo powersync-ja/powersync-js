5ada0837c293a4762a6d08dde54814f9dffa7d23
stevensJourney <steven@journeyapps.com>
2025-11-13 09:40:33 +0200

    fix: capacitor readTransaction  not allowed errors

--- packages/capacitor/src/adapter/CapacitorSQLiteAdapter.ts
+++ packages/capacitor/src/adapter/CapacitorSQLiteAdapter.ts
@@ -120,20 +120,29 @@ export class CapacitorSQLiteAdapter extends BaseObserver<DBAdapterListener> impl
   protected generateLockContext(db: SQLiteDBConnection): LockContext {
     const _execute = async (query: string, params: any[] = []): Promise<QueryResult> => {
       const platform = Capacitor.getPlatform();
+
+      const _query = async (query: string, params: any[] = []) => {
+        const result = await db.query(query, params);
+        const arrayResult = result.values ?? [];
+        return {
+          rowsAffected: 0,
+          rows: {
+            _array: arrayResult,
+            length: arrayResult.length,
+            item: (idx: number) => arrayResult[idx]
+          }
+        };
+      };
+
+      if (db.getConnectionReadOnly()) {
+        return _query(query, params);
+      }
+
       if (platform == 'android') {
         // Android: use query for SELECT and executeSet for mutations
         // We cannot use `run` here for both cases.
         if (query.toLowerCase().trim().startsWith('select')) {
-          const result = await db.query(query, params);
-          const arrayResult = result.values ?? [];
-          return {
-            rowsAffected: 0,
-            rows: {
-              _array: arrayResult,
-              length: arrayResult.length,
-              item: (idx: number) => arrayResult[idx]
-            }
-          };
+          return _query(query, params);
         } else {
           const result = await db.executeSet([{ statement: query, values: params }], false);
           return {
