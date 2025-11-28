34296fd0aac6e745a7a9719810ac30a7e096e836
LucDeCaf <luc@powersync.com>
2025-11-28 14:41:35 +0200

    fix rn bundling not working properly

--- demos/react-native-supabase-todolist/metro.config.js
+++ demos/react-native-supabase-todolist/metro.config.js
@@ -10,11 +10,9 @@ const config = getDefaultConfig(projectRoot);
 // 1. Watch all files within the monorepo
 config.watchFolders = [projectRoot];
 // 2. Let Metro know where to resolve packages and in what order
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-];
+config.resolver.nodeModulesPaths = [path.resolve(projectRoot, 'node_modules')];
 // #3 - Force resolving nested modules to the folders below
-config.resolver.disableHierarchicalLookup = true;
-config.resolver.unstable_enableSymlinks = true;
+// config.resolver.disableHierarchicalLookup = true;
+// config.resolver.unstable_enableSymlinks = true;
 
 module.exports = config;
