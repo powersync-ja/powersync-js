21fbf58fb37cdf2832d797114df265584119c3ce
LucDeCaf <luc@powersync.com>
2025-12-12 14:23:15 +0200

    Final final touches

--- demos/react-native-web-supabase-todolist/metro.config.js
+++ demos/react-native-web-supabase-todolist/metro.config.js
@@ -2,19 +2,7 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('node:path');
 
-// Find the project and workspace directories
-const projectRoot = __dirname;
 const config = getDefaultConfig(projectRoot);
-
-// 1. Watch all files within the monorepo
-config.watchFolders = [projectRoot];
-// 2. Let Metro know where to resolve packages and in what order
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-];
-// #3 - Force resolving nested modules to the folders below
-// config.resolver.disableHierarchicalLookup = true;
-// config.resolver.unstable_enableSymlinks = true;
 
 // Needed to make `@powersync/web/umd` imports work
 config.resolver.unstable_enablePackageExports = true;
