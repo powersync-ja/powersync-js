18944831183d34f425f2c548e48527fc4efbacc6
stevensJourney <steven@journeyapps.com>
2026-01-12 17:40:15 +0200

    try restore test sequentialism

--- packages/node/vitest.config.ts
+++ packages/node/vitest.config.ts
@@ -5,10 +5,6 @@ export default defineConfig({
   test: {
     silent: false,
     // This doesn't make the tests considerably slower. It may improve reliability for GH actions.
-    fileParallelism: false,
-    sequence: {
-      concurrent: false
-    },
-    testTimeout: 20_000
+    fileParallelism: false
   }
 });
