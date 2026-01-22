7d76b5b71f547d82dc482be7344a49741cb25df6
stevensJourney <steven@journeyapps.com>
2026-01-22 10:48:35 +0200

    fixes for some demos

--- demos/example-vite/vite.config.ts
+++ demos/example-vite/vite.config.ts
@@ -14,8 +14,7 @@ export default defineConfig({
   optimizeDeps: {
     // Don't optimize these packages as they contain web workers and WASM files.
     // https://github.com/vitejs/vite/issues/11672#issuecomment-1415820673
-    exclude: ['@powersync/web'],
-    include: []
+    exclude: ['@powersync/web']
   },
   worker: {
     format: 'es'
