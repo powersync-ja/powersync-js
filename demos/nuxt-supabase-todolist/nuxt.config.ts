15eb12a986f035b4c4865df03f1340c2ae9de847
Amine <amine@powersync.com>
2026-02-25 17:40:14 +0800

    chore: Update pnpm-lock.yaml and refactor nuxt.config.ts

--- demos/nuxt-supabase-todolist/nuxt.config.ts
+++ demos/nuxt-supabase-todolist/nuxt.config.ts
@@ -1,23 +1,17 @@
 export default defineNuxtConfig({
-
-  modules: [
-    '@powersync/nuxt',
-    '@nuxt/eslint',
-    '@nuxt/ui',
-    '@nuxtjs/supabase',
-  ],
+  modules: ['@powersync/nuxt', '@nuxt/eslint', '@nuxt/ui', '@nuxtjs/supabase'],
   ssr: false,
 
   devtools: {
-    enabled: true,
+    enabled: true
   },
 
   css: ['~/assets/css/main.css'],
 
   runtimeConfig: {
     public: {
-      powersyncUrl: process.env.NUXT_PUBLIC_POWERSYNC_URL,
-    },
+      powersyncUrl: process.env.NUXT_PUBLIC_POWERSYNC_URL
+    }
   },
 
   // enable hot reloading when we make changes to our module
@@ -27,30 +21,28 @@ export default defineNuxtConfig({
 
   vite: {
     optimizeDeps: {
-      exclude: ['@journeyapps/wa-sqlite', '@powersync/web'],
-      include: [
-        '@supabase/postgrest-js',
-      ],
+      exclude: ['@powersync/web'],
+      include: ['@supabase/postgrest-js']
     },
 
     worker: {
-      format: 'es',
-    },
+      format: 'es'
+    }
   },
 
   unocss: {
-    autoImport: false,
+    autoImport: false
   },
 
   eslint: {
     config: {
-      stylistic: true,
-    },
+      stylistic: true
+    }
   },
 
   powersync: {
     useDiagnostics: true,
-    kysely: true,
+    kysely: true
   },
 
   supabase: {
@@ -59,12 +51,12 @@ export default defineNuxtConfig({
     redirectOptions: {
       login: '/login',
       callback: '/confirm',
-      exclude: ['/unprotected', '/public/*'],
+      exclude: ['/unprotected', '/public/*']
     },
     clientOptions: {
       auth: {
-        persistSession: true,
-      },
-    },
-  },
-})
+        persistSession: true
+      }
+    }
+  }
+});
