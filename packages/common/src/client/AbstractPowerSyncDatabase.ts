1686d8391ff6e9c7fe8005cbd7b79e9470aab8b0
Simon Binder <simon@journeyapps.com>
2026-03-16 13:08:01 +0100

    Fix readTransaction implementation

--- packages/common/src/client/AbstractPowerSyncDatabase.ts
+++ packages/common/src/client/AbstractPowerSyncDatabase.ts
@@ -925,7 +925,7 @@ SELECT * FROM crud_entries;
     await this.waitForReady();
     return this.database.readTransaction(
       async (tx) => {
-        const res = await callback({ ...tx });
+        const res = await callback(tx);
         await tx.rollback();
         return res;
       },
