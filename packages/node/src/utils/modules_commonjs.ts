41aa4ad227d7498c9310ec0f8b3abc2ed3b7e275
Simon Binder <simon@journeyapps.com>
2026-06-09 10:39:10 +0200

    Node: Remove CommonJS build

--- packages/node/src/utils/modules_commonjs.ts
+++
@@ -1,7 +0,0 @@
-// NOTE! Do not import this file directly! We have a rollup plugin that rewrites imports to modules.js to this file when
-// bundling to CommonJS.
-export const isBundledToCommonJs: boolean = true;
-
-export async function dynamicImport(path: string) {
-  return require(path);
-}
