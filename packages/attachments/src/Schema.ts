37221948ff267957d0b0b6c3ecd75a604abe5784
Simon Binder <simon@journeyapps.com>
2026-06-24 09:01:01 +0200

    Revert legacy attachment changes

--- packages/attachments/src/Schema.ts
+++ packages/attachments/src/Schema.ts
@@ -1,4 +1,4 @@
-import { ColumnsType, Table, column, TableOptions } from '@powersync/common';
+import { Column, ColumnType, ResolvedTable, ResolvedTableOptions, Table } from '@powersync/common';
 
 export const ATTACHMENT_TABLE = 'attachments';
 
@@ -20,31 +20,27 @@ export enum AttachmentState {
   ARCHIVED = 4 // Attachment has been orphaned, i.e. the associated record has been deleted
 }
 
-export interface AttachmentTableOptions extends Omit<TableOptions, 'name' | 'columns'> {
+export interface AttachmentTableOptions extends Omit<ResolvedTableOptions, 'name' | 'columns'> {
   name?: string;
-  additionalColumns?: ColumnsType;
+  additionalColumns?: Column[];
 }
 
-export class AttachmentTable extends Table {
+export class AttachmentTable extends ResolvedTable {
   constructor(options?: AttachmentTableOptions) {
-    const { additionalColumns = {}, ...tableOptions } = options ?? {};
-
-    super(
-      {
-        filename: column.text,
-        local_uri: column.text,
-        timestamp: column.integer,
-        size: column.integer,
-        media_type: column.text,
-        state: column.integer,
-        ...additionalColumns
-      },
-      {
-        name: ATTACHMENT_TABLE,
-        ...tableOptions,
-        localOnly: true,
-        insertOnly: false
-      }
-    );
+    super({
+      ...options,
+      name: options?.name ?? ATTACHMENT_TABLE,
+      localOnly: true,
+      insertOnly: false,
+      columns: [
+        new Column({ name: 'filename', type: ColumnType.TEXT }),
+        new Column({ name: 'local_uri', type: ColumnType.TEXT }),
+        new Column({ name: 'timestamp', type: ColumnType.INTEGER }),
+        new Column({ name: 'size', type: ColumnType.INTEGER }),
+        new Column({ name: 'media_type', type: ColumnType.TEXT }),
+        new Column({ name: 'state', type: ColumnType.INTEGER }), // Corresponds to AttachmentState
+        ...(options?.additionalColumns ?? [])
+      ]
+    });
   }
 }
