a6bf7e5a5610b485cb3b7de9bd53511b686ebad9
Simon Binder <simon@journeyapps.com>
2026-06-26 12:25:48 +0200

    Fix tests

--- packages/react-native/tests/sync/ReactNativeRemote.test.ts
+++ packages/react-native/tests/sync/ReactNativeRemote.test.ts
@@ -1,7 +1,8 @@
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it, Mock, vi } from 'vitest';
 
-import { ReactNativeRemote } from '../../src/sync/stream/ReactNativeRemote';
+import { PowerSyncFetchImplementation, ReactNativeRemote } from '../../src/sync/stream/ReactNativeRemote';
 import { createConsoleLogger } from '@powersync/common';
+import { FetchOptions } from '@powersync/shared-internals';
 
 const connector = {
   fetchCredentials: async () => ({
@@ -20,25 +21,39 @@ function jsonResponse(body: unknown): Response {
   } as Response;
 }
 
+function mockFetchImplementation(
+  options?: Partial<PowerSyncFetchImplementation>
+): [PowerSyncFetchImplementation, Mock<(options: FetchOptions) => Promise<Response>>] {
+  const fetch = vi.fn(async (_options: FetchOptions) => jsonResponse({ ok: true }));
+
+  const psFetchImpl: PowerSyncFetchImplementation = {
+    supportsStreams: true,
+    run: fetch,
+    ...options
+  };
+  return [psFetchImpl, fetch];
+}
+
 const logger = createConsoleLogger();
 
 describe('ReactNativeRemote', () => {
   it('does not use streaming for non-streaming GET requests', async () => {
-    const fetchImplementation = vi.fn(async () => jsonResponse({ ok: true }));
+    const [fetchImplementation, mock] = mockFetchImplementation();
     const remote = new ReactNativeRemote(connector, logger, { fetchImplementation });
 
     await remote.get('/write-checkpoint2.json');
 
-    expect(fetchImplementation).toHaveBeenCalledWith(
-      'https://example.com/write-checkpoint2.json',
-      expect.objectContaining({
+    expect(mock).toHaveBeenCalledWith({
+      resource: 'https://example.com/write-checkpoint2.json',
+      request: expect.objectContaining({
         method: 'GET'
-      })
-    );
+      }),
+      expectStreamingResponse: false
+    });
   });
 
   it('uses text streaming for non-streaming POST requests', async () => {
-    const fetchImplementation = vi.fn(async () => jsonResponse({ ok: true }));
+    const [fetchImplementation, mock] = mockFetchImplementation();
     const remote = new ReactNativeRemote(connector, logger, { fetchImplementation });
 
     try {
@@ -51,14 +66,27 @@ describe('ReactNativeRemote', () => {
       // This fails because the mock is incomplete, ignore.
     }
 
-    expect(fetchImplementation).toHaveBeenCalledWith(
-      'https://example.com/sync/stream',
-      expect.objectContaining({
-        method: 'POST',
-        reactNative: expect.objectContaining({
-          textStreaming: true
-        })
+    expect(mock).toHaveBeenCalledWith({
+      resource: 'https://example.com/sync/stream',
+      request: expect.objectContaining({
+        method: 'POST'
+      }),
+      expectStreamingResponse: true
+    });
+  });
+
+  it('throws when attempting to stream with unsupported fetch', async () => {
+    const [fetchImplementation, mock] = mockFetchImplementation({ supportsStreams: false });
+    const remote = new ReactNativeRemote(connector, logger, { fetchImplementation });
+
+    await expect(
+      remote.fetchStream({
+        path: '/sync/stream',
+        data: {},
+        abortSignal: new AbortController().signal
       })
+    ).rejects.toThrow(
+      'The PowerSync SDK requires a fetch() implementation capable of streaming responses, which React Native does not support natively.'
     );
   });
 });
--- packages/react-native/vitest.config.ts
+++ packages/react-native/vitest.config.ts
@@ -4,8 +4,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   resolve: {
     alias: {
-      'react-native': path.resolve(__dirname, './tests/mocks/react-native.ts'),
-      'react-native-fetch-api': path.resolve(__dirname, './tests/mocks/react-native-fetch-api.ts')
+      'react-native': path.resolve(__dirname, './tests/mocks/react-native.ts')
     }
   },
   test: {
