c2fa06b563fe0d53797d62a8505ce1a37276f693
Simon Binder <simon@powersync.com>
2026-08-13 13:35:09 +0200

    Maintenance items and release fixes (#1067)

--- tools/import-tests/vitest.config.ts
+++ tools/import-tests/vitest.config.ts
@@ -9,7 +9,7 @@ const config: ViteUserConfig = {
   optimizeDeps: {
     // Don't optimise these packages as they contain web workers and WASM files.
     // https://github.com/vitejs/vite/issues/11672#issuecomment-1415820673
-    exclude: ['@journeyapps/wa-sqlite', '@powersync/web'],
+    exclude: ['@journeyapps/wa-sqlite', '@powersync/web', 'comlink'],
     include: []
   },
   plugins: [],
